910a7e52ad2bfdb3c9232565c16889a79a63e38a
Rohit Sharma <66187098+rsharmarohit@users.noreply.github.com>
2021-03-11 17:55:16 -0500

    Create division.py

---
+++ division.py
@@ -0,0 +1,18 @@
+# Task
+# The provided code stub reads two integers, a and b, from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division, a //b . The second line should contain the result of float division,  / .
+
+# No rounding or formatting is necessary.
+
+
+
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    divide = a//b
+    mod = a/b
+    print (divide)
+    print (mod)
