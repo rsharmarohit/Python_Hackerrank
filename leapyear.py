13867dd80320635fc124d46d2f6766c70cafd6cd
Rohit Sharma <66187098+rsharmarohit@users.noreply.github.com>
2021-03-11 17:49:22 -0500

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,17 @@
+## Task
+
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+
+def is_leap(year):
+    #leap = False
+    
+    # Write your logic here
+ if(year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+        leap = True
+ else:
+    leap = False   
+ return leap
+ 
+year = int(input())
+print(is_leap(year))
