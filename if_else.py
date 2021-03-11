9139c006933bdd37cfc04f804a877397d8194122
Rohit Sharma <66187098+rsharmarohit@users.noreply.github.com>
2021-03-11 17:50:38 -0500

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,19 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
